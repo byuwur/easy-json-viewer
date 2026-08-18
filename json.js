e5b8abd3283930ebc0446a6bad5dd81f9dc612d6
Andrés Trujillo Mateus <atrujillomateus@gmail.com>
2026-08-18 06:12:37 -0500

    [Update] (c) year

--- json.js
+++ json.js
@@ -3,7 +3,7 @@
  * File: json.js
  * Desc: Contains the heart of easy JSON viewer.
  * Deps: none
- * Copyright (c) 2024 Andrés Trujillo [Mateus] byUwUr
+ * Copyright (c) 2026 Andrés Trujillo [Mateus] byUwUr
  * https://github.com/byuwur/easy-json-viewer
  */
 
